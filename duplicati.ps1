97182ff2973d24eb6723fd4d1670f8daba9ad0c4
TS-Steff <smueller@ts-management.ch>
2018-05-01 11:35:36 +0200

    UseBasicParsing for Invoke-WebRequest

--- duplicati.ps1
+++ duplicati.ps1
@@ -26,6 +26,7 @@ $headers = @{
 "Upgrade-Insecure-Requests"="1";
 }
 
+Invoke-WebRequest -Uri $url -Method GET -Headers $headers -SessionVariable SFSession -UseBasicParsing | Out-Null
 
 
 #Gets required tokens
